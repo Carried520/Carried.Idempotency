7cbfaaea6e2356edbc8341f8befe0690be025c98
Carried <ostrowski.cz@gmail.com>
2026-09-04 23:44:00 +0200

    feat(aspnet): add controller idempotency support

---
+++ tests/Carried.Idempotency.AspNet.Tests/TestOrdersControllers.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Carried.Idempotency.AspNet.Tests;
+
+[ApiController]
+[Route("test-controller/orders")]
+public sealed class TestOrdersController : ControllerBase
+{
+    private static int _methodInvocationCount;
+
+    public static int MethodInvocationCount =>
+        Volatile.Read(ref _methodInvocationCount);
+
+    public static void Reset()
+    {
+        Interlocked.Exchange(
+            ref _methodInvocationCount,
+            0);
+    }
+
+    [HttpPost]
+    [RequireIdempotency]
+    public IActionResult Create()
+    {
+        Interlocked.Increment(
+            ref _methodInvocationCount);
+
+        return Created(
+            "/test-controller/orders/123",
+            new
+            {
+                Id = 123
+            });
+    }
+}
